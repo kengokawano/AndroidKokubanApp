2656aa29b875d87dbee3fc5295c041ff0e0157b8
orange <orangeman2002@gmail.com>
2025-09-29 23:06:21 +0900

    ver 5 fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "jp.saitama.orange.drawkokuban2"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2"
+        versionCode = 5
+        versionName = "1.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
